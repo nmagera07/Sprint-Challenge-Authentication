679014697cbd9e1328ecf6fb4243190c1a594ebe
Nate Magera <nmagera07@gmail.com>
2019-08-23 11:32:53 -0400

    added login route

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -23,7 +23,21 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  let { username, password } = req.body
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+        res.status(200).json({ message: `Welcome to the app, ${user.username}`, token})
+      } else {
+        res.status(401).json({ message: 'You shall not pass!!'})
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Failed to login!', error})
+    })
 });
 
 
